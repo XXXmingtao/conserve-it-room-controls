9d3ef2261978e0c9dcd16abb6d2fa73b26d1bf60
Mingtao Liu <s3714710@student.rmit.edu.au>
2023-01-12 14:18:28 +1100

    Add toString method to printout temperature, heater and airCon status

--- src/main/java/org/conserve/Apartment.java
+++ src/main/java/org/conserve/Apartment.java
@@ -22,4 +22,8 @@ public class Apartment extends Rooms {
     public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
     }
+
+    public String toString() {
+        return String.format("AptNumber: %s - Current Temperature: %.1f °C - Heater: %b - Air Conditioner: %b", this.getRoomID(), this.getTemperature(), this.isHeaterStatus(), this.isAirConStatus());
+    }
 }
--- src/main/java/org/conserve/Main.java
+++ src/main/java/org/conserve/Main.java
@@ -3,7 +3,10 @@ package org.conserve;
 public class Main {
     public static void main(String[] args) {
         Apartment apt1 = new Apartment("Mingtao", 101, false, false);
+        Apartment apt2 = new Apartment("Mingtao", 102, false, false);
 
+        System.out.println(apt1);
+        System.out.println(apt2);
     }
 
 }
